1828fb6488f11a38e206b204a0bc71757790c623
ivannikovmn <local_host@mail.ru>
2023-07-17 15:44:00 +0600

    add RestAPI l-7 (add resume [models and migrations])

---
+++ app/resume/models/Resume.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../../config/db') //Импортируйте настройки подключения к базе данных
+const City = require('../../region/City')
+const User = require('../../auth/User')
+// const Country = require('../../region/Country')
+
+const Resume = sequelize.define('Resume', {
+  post: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },  
+  stories: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },    
+  about: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },   
+  participants: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },  
+  followers: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },         
+});
+
+Resume.belongsTo(City, { foreignKey: 'cityId' }); // Определяем внешний ключ 'roleId'
+Resume.belongsTo(User, { foreignKey: 'userId' }); // Определяем внешний ключ 'roleId'
+// Resume.belongsTo(Country, { foreignKey: 'citizenship' }); // Определяем внешний ключ 'roleId'
+
+module.exports = Resume;
---
+++ migrations/20230717092458-create-resume.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Resumes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      post: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      stories: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      about: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      participants: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      followers: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Cities',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Resumes');
+  }
+};
