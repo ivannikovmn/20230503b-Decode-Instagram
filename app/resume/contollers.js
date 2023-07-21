e51213b086fbd3811ee3eb609007b8239b65b0f4
ivannikovmn <local_host@mail.ru>
2023-07-21 19:32:57 +0600

    add RestAPI l-9 (resume: p1 list, p2 delete, p3 edit)

--- app/resume/contollers.js
+++ app/resume/contollers.js
@@ -16,6 +16,54 @@ const createResume = async (req, res) => {
     res.status(200).send(resume);
 }
 
+const getMyResumes = async (req, res) => {
+    const resumes = await Resume.findAll({where: {userId: req.user.id}});
+    res.status(200).send(resumes)
+}
+
+const getAllResumes = async (req, res) => {
+    const resumes = await Resume.findAll();
+    res.status(200).send(resumes)
+}
+
+const getResume = async (req, res) => {
+    const resume = await Resume.findByPk(req.params.id);
+    res.status(200).send(resume)
+}
+
+const deleteResume = async (req, res) => {
+    const data = await Resume.destroy({
+        where: {
+            id: req.params.id,
+        }
+    })
+    console.log(data);
+    res.status(200).end()
+}
+
+const editResume = async (req, res) => {
+    await Resume.update({
+        post: req.body.post,      
+        story: req.body.story,  
+        about: req.body.about,
+        participants: req.body.participants,
+        cityId: req.body.cityId,
+        userId: req.user.id      
+    },
+    {
+        where: {
+        id: req.body.id
+        }
+    })
+    
+    res.status(200).end()
+}
+
 module.exports = {
-    createResume
+    createResume,
+    getMyResumes,
+    getAllResumes,
+    getResume,
+    deleteResume,
+    editResume
 }
--- app/resume/routes.js
+++ app/resume/routes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router();
-const {createResume} = require('./contollers')
+const {createResume, getMyResumes, getAllResumes, getResume, deleteResume, editResume} = require('./contollers')
 const {isClient} = require('../auth/middlewares')
 const passport = require('passport')
-const {validateResume} = require('./middlewares')
+const {validateResume, isAuthorOfResume} = require('./middlewares')
 
 // router.post('/api/resume', passport.authenticate('jwt', { session: false }), isEmployee, validateResume, createResume)
 router.post('/api/resume', passport.authenticate('jwt', { session: false }), isClient, validateResume, createResume)
+router.get('/api/resume', passport.authenticate('jwt', { session: false }), isClient, getMyResumes)
+router.get('/api/resumes', passport.authenticate('jwt', { session: false }), isClient, getAllResumes)
+router.get('/api/resume/:id', passport.authenticate('jwt', { session: false }), getResume)
+router.delete('/api/resume/:id', passport.authenticate('jwt', { session: false }), isClient, isAuthorOfResume, deleteResume) 
+router.put('/api/resume', passport.authenticate('jwt', { session: false }), isClient, isAuthorOfResume, validateResume, editResume) 
 
 module.exports = router;
