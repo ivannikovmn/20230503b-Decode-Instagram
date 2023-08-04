a50beeca0fb12a94ceb54c043ec380f9c2cac648
ivannikovmn <local_host@mail.ru>
2023-08-04 17:34:51 +0600

    fix router post

--- app/resume/routes.js
+++ app/resume/routes.js
@@ -8,15 +8,15 @@ const passport = require('passport')
 const {validateResume, isAuthorOfResume} = require('./middlewares')
 
 // router.post('/api/resume', passport.authenticate('jwt', { session: false }), isEmployee, validateResume, createResume)
-router.post('/api/resume', passport.authenticate('jwt', { session: false }), isClient, validateResume, createResume)
-router.get('/api/resume', passport.authenticate('jwt', { session: false }), isClient, getMyResumes)
-router.get('/api/resumes', passport.authenticate('jwt', { session: false }), isClient, getAllResumes)
+router.post('/api/posts', passport.authenticate('jwt', { session: false }), isClient, validateResume, createResume)
+router.get('/api/posts/my', passport.authenticate('jwt', { session: false }), isClient, getMyResumes)
+router.get('/api/posts', passport.authenticate('jwt', { session: false }), isClient, getAllResumes)
 
 router.get('/api/posts/byUsername/:username', passport.authenticate('jwt', { session: false }), isClient, getUsernameResumes)
 
-router.get('/api/resume/:id', passport.authenticate('jwt', { session: false }), getResume)
-router.delete('/api/resume/:id', passport.authenticate('jwt', { session: false }), isClient, isAuthorOfResume, deleteResume) 
-router.put('/api/resume', passport.authenticate('jwt', { session: false }), isClient, isAuthorOfResume, validateResume, editResume) 
+router.get('/api/post/:id', passport.authenticate('jwt', { session: false }), getResume)
+router.delete('/api/post/:id', passport.authenticate('jwt', { session: false }), isClient, isAuthorOfResume, deleteResume) 
+router.put('/api/post', passport.authenticate('jwt', { session: false }), isClient, isAuthorOfResume, validateResume, editResume) 
 
 router.post('/api/story', passport.authenticate('jwt', { session: false }), isClient, createStory)
 router.get('/api/resumesBefore24hours', passport.authenticate('jwt', { session: false }), isClient, getAllResumesBefore24hours)
