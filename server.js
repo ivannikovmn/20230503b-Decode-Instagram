fd19141cb1e0eed9df9dd4126d9db83b77011036
ivannikovmn <local_host@mail.ru>
2023-09-07 19:55:26 +0600

    fix CORS

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(logger('dev'))
 app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
+
 app.use(express.urlencoded())
 // app.use(upload.any()) 
 app.use(express.json())
